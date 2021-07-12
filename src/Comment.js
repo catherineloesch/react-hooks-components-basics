61202d3397c9ef3af25b59753804fbf47a26b2df
Catherine Loesch <catherine.loesch@protonmail.com>
2021-07-12 18:15:18 +0100

    no test/instructions in file

--- src/Comment.js
+++ src/Comment.js
@@ -5,3 +5,15 @@ function Comment() {
 }
 
 export default Comment;
+
+// new components are written as function components
+// but: class components will continue to be supported as well
+
+// class Comment extends React.Component {
+//   render() {
+//     return <div>Naturally, I agree with this article.</div>;
+//   }
+// } 
+
+
+
